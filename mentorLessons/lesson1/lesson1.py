cc84a283e3edde87c9b819e07ec00b468b4dcc64
Gulnar Mammadzada <gulnarmmmdzad05@gmail.com>
2026-06-04 15:08:11 +0400

    initial commit

---
+++ mentorLessons/lesson1/lesson1.py
@@ -0,0 +1,63 @@
+#data tipler-string,integer,float,boolean
+# ad="Eli"   #string
+# yash=20    #integer-tam ededler
+# qiymet=27.5  #float
+# yashil_ishiq=True
+# qirmizi_ishiq=False
+#
+# Ad
+# _ad
+# ad
+# ad1
+# ad2
+# telebe_adi
+# telebeAdi
+# telebe adi
+# telebe@!#\
+# 1ad
+
+
+
+# ad="Eli"
+# print(ad)
+# print("salam",ad,"necesen")
+# print(f"salam {ad} necesen ")   #f-string
+
+
+
+
+# a=10
+# a+=2
+# a=a+2
+# print(a)
+
+# print(a+b,a-b,a*b)
+# print(a-b)
+# print(a*b)
+# print(a**b)
+
+#and or  not
+#and - iki sert,her iki sert True olsa cavab True
+#or -iki sert,1 sert True cavab True
+# a=8
+# b=4
+# print(a>9 and b>3)
+# print(a>9 or b>3)
+
+# yashil=True
+# print(not yashil)
+
+# ad="eli"
+# print(ad)
+#
+# a=5
+#
+# ad=input("Adiniz nedir?")
+# print(ad)
+#
+
+
+# ad=input("Adiniz nedir?")
+# yash=int(input("yashiniz necedir?"))
+# qiymet=float(input("Alma neceyedir?"))
+# print(qiymet)
