116598ac44cb955e004c171252dc3cd31923d024
Gulnar Mammadzada <gulnarmmmdzad05@gmail.com>
2026-06-10 17:31:31 +0400

    lesson 2 added

---
+++ mentorLessons/lesson2/lesson2.py
@@ -0,0 +1,20 @@
+# #if, elif ,else  - sert operatorlari
+#
+# a=5
+# b=5
+#
+# if a>b:
+#     print("A B-den boyukdur")
+# else:
+#     print("B A-dan boyukdur")
+#
+#
+# imtahan_bali=92
+#
+# if imtahan_bali>90:
+#     print("Siz elachisiz")
+# elif imtahan_bali>=80:
+#     print("Siz zerbecisiz")
+# else:
+#     print("Siz kesilmisiz")
+#
