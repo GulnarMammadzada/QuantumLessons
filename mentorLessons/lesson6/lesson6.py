04f3a68c1458c50484d49ae739805d0f58793427
Gulnar Mammadzada <gulnarmmmdzad05@gmail.com>
2026-06-18 20:19:44 +0400

    update

--- mentorLessons/lesson6/lesson6.py
+++ mentorLessons/lesson6/lesson6.py
@@ -47,9 +47,45 @@
 # if seher.lower() == "baku":
 #     print("Paytaxta xoş gəlmisiniz!")
 
+#
+# sinif = [75, 90, 55]
+# sinif.sort()
+# print(sinif)
+# sinif.sort(reverse=True)
+# print(sinif)
 
-sinif = [75, 90, 55]
-sinif.sort()
-print(sinif)
-sinif.sort(reverse=True)
-print(sinif)
+
+
+
+# xercler=[34,64,12,43]
+#
+# vip=[]
+#
+# for xerc in xercler:
+#     if xerc>30:
+#         vip.append(xerc)
+# print(vip)
+
+
+#
+# hefte_1 = [15, 45, 8]
+# hefte_2 = [60, 12, 35, 90]
+#
+# butun_xercler = hefte_1 + hefte_2
+#
+# butun_xercler.sort(reverse=True)
+#
+# boyuk_xercler=[]
+#
+# for xerc in butun_xercler:
+#     if xerc>30:
+#         boyuk_xercler.append(xerc)
+
+
+print("30 AZN-dən çox olan xərclər:", butun_xercler)
+print("Sayı:", len(butun_xercler))
+
+
+
+# print("30 AZN-dən çox olan xərclər:", böyük_xərclər)
+# print("Sayı:", len(böyük_xərclər))
