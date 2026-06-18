f87eedbfc78f28e850576abe7d08e80dd1d9284c
Gulnar Mammadzada <gulnarmmmdzad05@gmail.com>
2026-06-18 20:04:24 +0400

    lesson6

---
+++ mentorLessons/lesson5/lesson5.py
@@ -0,0 +1,64 @@
+# meyve="alma"
+#
+#
+# reqemler=[3,8,7,2]
+# reqemler.sort()
+# print(reqemler)
+
+
+
+# meyveler.reverse()
+# print(meyveler)
+#
+# reqemler=[1,3,-4]
+# reqemler.reverse()
+# print(reqemler)
+
+
+
+
+
+
+
+
+# meyveler.pop()
+# meyveler.pop()
+# meyveler.remove("nar")
+
+
+# meyveler.insert(1,"erik")
+# meyveler.append("uzum")
+
+
+#index-elementin sira nomresi
+
+#
+#
+# meyveler2=("alma","banan")
+# print(meyveler2[0:2])
+#
+#
+
+meyveler=["alma","banan"]
+meyveler[0]="nar"
+print(meyveler)
+
+
+
+#
+# meyveler=("alma","banan")
+#
+# for meyve in meyveler:
+#     print(meyve)
+
+
+# kvadratlar=[]
+#
+# for i in range(5):
+#     kvadratlar.append(i*i)
+# print(kvadratlar)
+
+kvadratlar=[i+i for i in range(5)]
+print(kvadratlar)
+
+
---
+++ mentorLessons/lesson6/lesson6.py
@@ -0,0 +1,55 @@
+# meyve="alma"
+#
+# if meyve=="alma":
+#     print("True")
+
+# adlar2=["Ali","Aynur","Kenan"]
+# print(adlar2[0])
+
+
+# meyveler=["alma","banan","armud","gilas"]
+#
+# for meyve in meyveler:
+#     print(meyve)
+#
+# adlar=[
+#     ["Əli",    "Vəli",   "Ayşə"],   # Sətir 0
+#     ["Leyla",  "Murad",  "Kənan"],  # Sətir 1
+#     ["Nigar",  "Elvin",  "Səid"]    # Sətir 2
+# ]
+#
+# for setir in adlar:
+#     for ad in setir:
+#         print(setir)
+#         print(ad)
+
+#
+# if "Əli" in adlar[0]:
+#     print("Var")
+
+
+# ad="murad"
+# print(ad.upper())
+
+# ad="MuRaD"
+# print(ad.lower())
+
+
+# cumle = "salam necesiniz?"
+# print(cumle.capitalize())
+#
+# ad_soyad = "orxan veliyev"
+# print(ad_soyad.title())
+
+
+# seher = input("Şəhər adı yazın: ") # İstifadəçi "BaKu" yazdı fərz edək
+#
+# if seher.lower() == "baku":
+#     print("Paytaxta xoş gəlmisiniz!")
+
+
+sinif = [75, 90, 55]
+sinif.sort()
+print(sinif)
+sinif.sort(reverse=True)
+print(sinif)
